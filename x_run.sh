a67a6da5dd6201e28a20b643181a8a82ecdc1e16
gl-coding <cool3stone@gmail.com>
2025-06-15 23:42:04 +0800

    fix

--- x_run.sh
+++ x_run.sh
@@ -6,12 +6,26 @@ function download_videos() {
     python download_videos.py --json video_list_export.json
 }
 
+function export_links() {
+    python download_videos.py --export-links
+}
+
+function download_from_links() {
+    python download_videos.py --download
+}
+
 case $1 in
     "server")
         run_server
         ;;
     "download")
         download_videos
+        ;;
+    "export")
+        export_links
+        ;;
+    "dlurl")
+        download_from_links
         ;;
     *)
         echo "Usage: $0 {server|download}"
